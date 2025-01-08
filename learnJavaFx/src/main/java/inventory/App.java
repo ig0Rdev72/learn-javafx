1c73ae27ca3ea154ce494bc89b43e907f65acffa
ig0Rdev72 <ig0rdev72@gmail.com>
2025-01-07 19:03:36 -0600

    Create basic hello world, made new Main class to allow JavaFx to run from the classpath instead of its module path

--- learnJavaFx/src/main/java/inventory/App.java
+++ learnJavaFx/src/main/java/inventory/App.java
@@ -1,10 +1,27 @@
 package inventory;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 /**
  * Hello world!
  */
-public class App {
+public class App extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+            launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Button btn1=new Button("Say, Hello World");  
+        StackPane root=new StackPane();  
+        root.getChildren().add(btn1);  
+        Scene scene=new Scene(root);     
+        primaryStage.setScene(scene);  
+        primaryStage.setTitle("First JavaFX Application");  
+        primaryStage.show();  
     }
 }
---
+++ learnJavaFx/src/main/java/inventory/resources/Main.java
@@ -0,0 +1,9 @@
+package inventory.resources;
+
+import inventory.App;
+
+public class Main {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
